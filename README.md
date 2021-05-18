e805c1a020d0083fa4ef37a67ae98d0e86642bd3
Nhan Cao <nhancv92@gmail.com>
2021-05-18 10:38:30 +0700

    feat: initial

---
+++ README.md
@@ -0,0 +1,8 @@
+```
+TELEGRAM_CHAT_ID=
+TELEGRAM_MESSAGE=
+TELEGRAM_BOT_TOKEN=
+curl -s https://raw.githubusercontent.com/nhancv/telegram_notification/master/telegram_notification.sh | bash /dev/stdin $TELEGRAM_CHAT_ID $TELEGRAM_MESSAGE $TELEGRAM_BOT_TOKEN
+```
+
+
---
+++ telegram_notification.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TELEGRAM_CHAT_ID=$1
+TELEGRAM_MESSAGE=$2
+TELEGRAM_BOT_TOKEN=$3
+curl -X POST \
+     -H "Content-Type: application/json" \
+     -d "{\"chat_id\": ${TELEGRAM_CHAT_ID}, \"text\": \"${TELEGRAM_MESSAGE}\", \"disable_notification\": true}" \
+     https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
