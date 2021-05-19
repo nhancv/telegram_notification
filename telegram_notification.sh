6eadd61654caec4ed571ae327b0b5620a5969e6b
Nhan Cao <nhancv92@gmail.com>
2021-05-19 07:48:04 +0700

    feat: update notify

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -4,5 +4,5 @@ TELEGRAM_MESSAGE=$2
 TELEGRAM_BOT_TOKEN=$3
 curl -X POST \
      -H "Content-Type: application/json" \
-     -d "{\"chat_id\": ${TELEGRAM_CHAT_ID}, \"text\": \"${TELEGRAM_MESSAGE}\", \"disable_notification\": true}" \
+     -d "{\"chat_id\": ${TELEGRAM_CHAT_ID}, \"text\": \"${TELEGRAM_MESSAGE}\", \"disable_notification\": false}" \
      https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
